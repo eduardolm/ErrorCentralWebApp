806a27de2fac1668a9556248ba8b1c68ca6a3e9d
Eduardo <lodi001@uol.com.br>
2020-08-18 20:57:12 -0300

    Initial commit

---
+++ precache-manifest.ef041e7f42f5b830b3fbd2b2172930ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9830a6fa86af39677ba47a42f812f771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a4b4b8bbf6520d706b3",
+    "url": "/static/css/main.83d90cfe.chunk.css"
+  },
+  {
+    "revision": "565cee2a91579a23ad55",
+    "url": "/static/js/2.601e6f30.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/2.601e6f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4b4b8bbf6520d706b3",
+    "url": "/static/js/main.1e56caa8.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "15e963dbcda9a0709c4818e2ecb206c5",
+    "url": "/static/media/logo-small.15e963db.png"
+  },
+  {
+    "revision": "8ff1c14c2a373ea07b225a056260c60f",
+    "url": "/static/media/logo.8ff1c14c.png"
+  }
+]);
